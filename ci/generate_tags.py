43341848916979a02a8365ce6a9bfd434fd9f028
Erik Swanson <erik@retailnext.net>
2023-05-24 17:59:52 -0700

    Fix tag generation
    *   Include the name of the image.
    *   Include the `-i` option when running Docker.
    
    Signed-off-by: Erik Swanson <erik@retailnext.net>

--- ci/generate_tags.py
+++ ci/generate_tags.py
@@ -2,8 +2,9 @@ import os
 
 import pkg_resources
 
-image_name = os.getenv('IMAGE')
+image_name = os.environ['IMAGE']
 version = pkg_resources.get_distribution('pproxy').parsed_version.base_version
 tags = ['latest', version]
 tags.extend([version[:n] for n in range(len(version)) if version[n] == '.'])
+tags = ['{}:{}'.format(image_name, tag) for tag in tags]
 print('tags=' + ','.join(tags))
